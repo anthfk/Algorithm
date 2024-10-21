f1877a09a4804117ede681b3c4b186b33a25391c
anthfk <96532905+anthfk@users.noreply.github.com>
2024-10-21 22:08:45 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 27.78 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,35 +3,40 @@ import java.util.LinkedList;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int time = 0;
-        int wTotal = 0;
-        Queue<Integer> q = new LinkedList<>();
-        
-        for(int t : truck_weights){
-            while(true){
-                if(q.isEmpty()){
-                    q.offer(t);
-                    wTotal += t;
+    int time = 0;
+    int sum = 0;
+    Queue<Integer> q = new LinkedList<>();
+
+    for(int t : truck_weights) {
+		while(true) {
+			if(q.isEmpty()) { // 비었을때
+				q.offer(t);
+				sum += t;
+				time++;
+                break;
+			}
+			else if(q.size() == bridge_length) { // 꽉찼을때
+				sum -= q.poll();
+				//time++;
+			}
+			else { // 공간있을때
+				if(sum+t<=weight) { // 다음꺼 가능 
+					q.offer(t);
+					time++;
+					sum += t;
+					break;
+				}
+				else {	// 다음꺼 불가능
+					q.offer(0);
+					//sum -= q.poll();
                     time++;
-                    break;
-                }
-                else if(q.size() == bridge_length){ // 다리 가득 찬 경우
-                    wTotal -= q.poll();
-                }
-                else { // 다리가 가득 차지않은 경우
-                    if(wTotal+t<=weight){
-                        q.offer(t);
-                        wTotal += t;
-                        time++;
-                        break;
-                    } else {
-                        q.offer(0);
-                        time++;
-                    }
-                }
-                
-            }
-        }        
-        return time+bridge_length;
+					//break;
+				}
+			}
+		}
+    }    
+	
+        
+       return time+bridge_length;
     }
 }
