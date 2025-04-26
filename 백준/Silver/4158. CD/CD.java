c651bc7bf4edce41991db489ad79c893d6e0bfa8
anthfk <96532905+anthfk@users.noreply.github.com>
2025-04-26 22:52:09 +0900

    [Silver V] Title: CD, Time: 868 ms, Memory: 279208 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -2,42 +2,40 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        
-        while(true){
-            int count = 0;
+        while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
             
-            if(N==0 && M==0) break;
+            if (N == 0 && M == 0) break;
             
-            HashSet<Integer> Ncd = new HashSet<>();
+            int[] A = new int[N];
+            int[] B = new int[M];
             
-            for(int i=0; i<N; i++){
-                int n = Integer.parseInt(br.readLine());
-                Ncd.add(n);
+            for (int i = 0; i < N; i++) {
+                A[i] = Integer.parseInt(br.readLine());
+            }
+            for (int i = 0; i < M; i++) {
+                B[i] = Integer.parseInt(br.readLine());
             }
             
-            for(int i=0; i<M; i++){
-                int m = Integer.parseInt(br.readLine());
-                if(Ncd.contains(m)){
+            int i = 0, j = 0, count = 0;
+            while (i < N && j < M) {
+                if (A[i] == B[j]) {
                     count++;
+                    i++;
+                    j++;
+                } else if (A[i] < B[j]) {
+                    i++;
+                } else {
+                    j++;
                 }
             }
             
             System.out.println(count);
-            
-            
         }
-        
-
-        
-        
     }
 }
-
